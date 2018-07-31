d382caf5aa19113eb840ed8f6dab5acb259f770f
Jim <james.burnette@ucr.edu>
2018-07-30 17:07:50 -0700

    fixed the fasta collapser

---
+++ get_fasta5.php
@@ -0,0 +1,64 @@
+<?php
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+
+
+//get primer numbers
+$plate = $_GET['plate'];
+$fp = $_GET['forward'];
+$rp = $_GET['reverse'];
+$database = $_GET['db'];
+$type = $_GET['type'];
+
+//$rp = "B1";
+//$fp = 'A1';
+//$plate = "A";
+//connect to Database
+$db = new PDO('sqlite:../databases/'.$database); 
+$select='';
+if ($type == "A") {
+	$select = "SELECT output.fasta_id, output.seq AS sequence FROM output WHERE output.rp = :rp AND output.fp = :fp ";
+
+} else {
+
+	$select = "SELECT fasta_id, sequence FROM collapsed WHERE rp = :rp AND fp = :fp ";
+
+
+}
+
+
+
+//get sequences using primers
+//$select = "SELECT seqs.name, seqs.sequence FROM output JOIN seqs on output.fasta_id = seqs.id WHERE output.plate_num = :plate AND output.rp = :rp AND output.fp = :fp ";
+
+$params = array(
+	":rp" => $rp,
+	":fp" => $fp
+);
+try {
+	$select_stmt = $db->prepare($select);
+	$result = $select_stmt->execute($params);
+} catch (PODExecption $ex) {
+	die ($ex);
+}
+$fasta ='';
+while($rows = $select_stmt->fetch()) {
+	print_r($rows);
+	$fasta .= ">". $rows['fasta_id'] . "\n" .wordwrap($rows['sequence'], 80, "\n", true) . "\n";
+}
+//echo $fasta;
+
+?>
+<html>
+<head>
+	<title>Sequences</title>
+	
+	<meta http-equiv="CACHE-CONTROL" content="NO-CACHE"/>
+</head>
+<body>
+<pre>
+<?php echo $fasta; ?>
+</pre>
+</body>
+</html>
